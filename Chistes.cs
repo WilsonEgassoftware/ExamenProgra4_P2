20519f7f430e76880a5b58e64ff772c800353cda
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-06-05 08:11:50 -0500

    Add files via upload

---
+++ Chistes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra4_P2
+{
+    class Chistes : ContentPage
+    {
+
+
+        public Chistes()
+        {
+            InitializeComponent();
+            Title = "Chistes";
+
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void CargarChistes()
+        {
+            try
+            {
+                var cliente = new HttpClient();
+                var chistes = await cliente.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+            }
+            catch
+            {
+                ChistesLabel.Text = "Error al cargar el chiste. Inténtalo de nuevo más tarde.";
+
+            }
+
+        }
+        private void ChistesLabel_Clicked(object sender, EventArgs e)
+        {
+            CargarChistes();
+        }
+
+        public class ChistesLabel
+        {
+            public static string Text { get; internal set; }
+            public string setup { get; internal set; }
+            public string punchline { get; internal set; }
+        }
+
+    }   }
