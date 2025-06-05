a07c2f38c79a49b1dd15b5f6da381c0387f6315c
laboratorio <laboratorio@UPOAULA10708>
2025-06-05 07:21:08 -0500

    Agregar archivos de proyecto.

---
+++ ExamenProgra4_P2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExamenProgra4_P2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ExamenProgra4_P2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenProgra4_P2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
